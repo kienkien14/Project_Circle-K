c3ed9e44215fb3e38496af304b0d493c0addbe0a
Nguyen Trung Kien <89972489+kienkien14@users.noreply.github.com>
2022-06-16 17:00:55 +0700

    reponsive commit

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// let btnThucAn = document.querySelector('.btn_ta')
+// let btnThucUong = document.querySelector('.btn_tu')
+
+// let lst_ta = document.querySelector('.list_ta')
+// let lst_tu = document.querySelector('.list_tu')
+
+// btnThucAn.addEventListener('click', function () {
+//     lst_ta.classList.add('active')
+//     lst_tu.classList.remove('active')
+// });
+
+
+// btnThucUong.addEventListener('click', function () {
+//     lst_ta.classList.remove('active')
+//     lst_tu.classList.add('active')
+// });
