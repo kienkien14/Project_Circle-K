637a02138e893145414b86e730621e4962d1f2e4
Nguyen Trung Kien <89972489+kienkien14@users.noreply.github.com>
2022-06-19 23:59:14 +0700

    login sign-up commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,42 @@
 //     lst_ta.classList.remove('active')
 //     lst_tu.classList.add('active')
 // });
+
+
+// Sign Up
+function signup(e) {
+    event.preventDefault();
+    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var user = {
+      username: username,
+      email: email,
+      password: password,
+    };
+    var json = JSON.stringify(user);
+    localStorage.setItem(username, json);
+    alert("Đăng ký thành công");
+  }
+
+  // Login
+  function login(e) {
+    event.preventDefault();
+    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var user = localStorage.getItem(username);
+    var data = JSON.parse(user);
+    if (!user) {
+      alert("Không tìm thấy username");
+    } else if (
+      username == data.username &&
+      email == data.email &&
+      password == data.password
+    ) {
+      alert("Đăng nhập thành công");
+      window.location.href = "admin.html";
+    } else {
+      alert("Đăng nhập thất bại");
+    }
+  }
